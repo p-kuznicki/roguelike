11149a100cd8932c75950f2f87f42e69fb8f2a4f
p-kuznicki <rej@tutanota.com>
2024-07-30 23:02:14 +0200

    before branching

--- rog.py
+++ rog.py
@@ -8,28 +8,29 @@ def main(stdscr):
 ####### CLASSES AND FUNCTIONS
 ##################################
 
-    def is_impassable (y,x):
-        return y > (len(mapcols)-1) or y < 0 or x > (len(mapcols[0])-1) or x < 0 or mapcols[y][x].solid
+    def is_beyond_map (y, x):
+        return y < 0 or x < 0 or y >= len(mapcols) or x >= len(mapcols[0]) 
 
     def is_blocked (y,x):
-          return y > (len(mapcols)-1) or y < 0 or x > (len(mapcols[0])-1) or x < 0 or mapcols[y][x].solid or mapcols[y][x].occupied
-                     
-    def draw_ground_at(y, x):
-        stdscr.addch(mapcols[y][x].loot.sign) if mapcols[y][x].loot else stdscr.addch(mapcols[y][x].sign)
-
-
+        return is_beyond_map(y,x) or mapcols[y][x].solid or mapcols[y][x].occupied
+    
+    
     
     class Terrain():
+    
         def __init__(self, name, sign, solid=False):
             self.name = name
             self.sign = sign
             self.solid = solid
             self.occupied = False
             self.loot = False
-
+            
+        def draw_ground(self):
+            stdscr.addch(self.loot.sign) if self.loot else stdscr.addch(self.sign)
 
         
     class Item():
+    
          def __init__(self, name, sign):
              self.name = name
              self.sign = sign
@@ -43,18 +44,20 @@ def main(stdscr):
             self.corpse_sign = '%'
             self.y = y
             self.x = x
-            
+        
+        def draw_if_visible(self, y , x):
+            if abs(x - player.x) + abs(y - player.y) < 13:
+                stdscr.move(y, x)
+                stdscr.addch(self.sign)
+        
         def complete_movement(self, directiony, directionx):
             stdscr.move(self.y, self.x)
-            stdscr.addch(mapcols[self.y][self.x].sign)            
+            mapcols[self.y][self.x].draw_ground()            
             mapcols[self.y][self.x].occupied = False
             mapcols[directiony][directionx].occupied = self
-            if abs(self.x - player.x) + abs(self.y - player.y) < 13:
-                stdscr.move(directiony, directionx)
-                stdscr.addch(self.sign)
+            self.draw_if_visible(directiony, directionx)
             self.x = directionx
             self.y = directiony
-            stdscr.move(player.y,player.x)
             
         def move_random(self):
             directiony = self.y + random.randint(-1,1)
@@ -62,13 +65,15 @@ def main(stdscr):
             if not is_blocked(directiony, directionx): self.complete_movement(directiony, directionx)
 
         def get_closer(self):
-            directionx = self.x                         # assume monster is on the same row as player
-            if self.x > player.x: directionx -=1        # if player is on earlier row adjust direction by -1
-            if self.x < player.x: directionx +=1        # if player is on later row adjust direction by +1
-            directiony = self.y                         # assume monster is on the same column as player
-            if self.y > player.y: directiony -=1        # if player is on earlier column adjust direction by -1
-            if self.y < player.y: directiony +=1        # if player is on later column adjust direction by +1
-            if directiony == player.y and  directionx == player.x: return	    	# if tries to go into player - do nothing
+            directionx = self.x                         # create direction and assume monster is on the same row as player
+            if self.x > player.x: directionx -=1        # if player is on earlier row adjust direction by x-1
+            elif self.x < player.x: directionx +=1      # if player is on later row adjust direction by x+1
+            directiony = self.y                         # create direction and assume monster is on the same column as player
+            if self.y > player.y: directiony -=1        # if player is on earlier column adjust direction by y-1
+            elif self.y < player.y: directiony +=1      # if player is on later column adjust direction by y+1
+            
+            if directiony == player.y and  directionx == player.x: return	    	# if tries to go into player - do nothing... for now ]:->
+            
             if is_blocked(directiony, directionx):					# if direct path blocked try other:
                 if player.x == directionx and not is_blocked(directiony, directionx+1): directionx +=1          # if is in the same column as player and diagonal right is open, go diag-right   
                 elif player.x == directionx and not is_blocked(directiony, directionx-1): directionx -=1        # if is in the same column as player and diagonal left is open, go diag-left
@@ -90,25 +95,26 @@ def main(stdscr):
 
 
     class Player():
+    
         def __init__(self, y, x):
             self.y = y
             self.x = x
             self.sign = '@'
             self.inventory = []
+            
         def move(self, modifier_y, modifier_x):
             new_y = self.y + modifier_y
             new_x = self.x + modifier_x
-            if is_impassable(new_y, new_x): return
+            if is_beyond_map(new_y, new_x) or mapcols[new_y][new_x].solid: return
             if mapcols[new_y][new_x].occupied:
                 mapcols[new_y][new_x].occupied.die()
-                stdscr.move(self.y,self.x)
                 return
-            draw_ground_at(self.y,self.x)
+            mapcols[self.y][self.x].draw_ground()
             self.y = new_y
             self.x = new_x
             stdscr.move(self.y,self.x)
             stdscr.addch(self.sign)
-            stdscr.move(self.y,self.x)
+            
         def get_loot(self):
             self.inventory.append(mapcols[self.y][self.x].loot)
             mapcols[self.y][self.x].loot = False
@@ -122,11 +128,11 @@ def main(stdscr):
     stdscr.clear()
     
     mapcols = []		# generate map
-    for i in range(20):
+    for i in range(25):
         mapcols.append([])
     
     for row in mapcols:         
-        for i in range(40):
+        for i in range(60):
              row.append(Terrain(name = 'rock', sign='#', solid = True)) if random.randint(0,3) == 0 else row.append(Terrain(name = 'grass', sign='_'))
     stdscr.move(0,0)
     
@@ -136,7 +142,7 @@ def main(stdscr):
         stdscr.addch('\n')
 
             
-    while True:                 # generate player
+    while True:                 # generate player and print
         y = random.randint(0,19)
         x = random.randint(0,39)
         if mapcols[y][x].solid or mapcols[y][x].occupied: continue
@@ -144,23 +150,20 @@ def main(stdscr):
             player = Player(y,x)
             stdscr.move(player.y, player.x)
             stdscr.addch(player.sign)
-            stdscr.move(player.y,player.x)
+            #stdscr.move(player.y,player.x)
             break
         
-    monsters = []               # generate monsters
-    while len(monsters) < 7:
-        y = random.randint(0,19)
-        x = random.randint(0,39)
+    monsters = []               # generate monsters and print if visible
+    while len(monsters) < len(mapcols)/2:
+        y = random.randint(0,len(mapcols)-1)
+        x = random.randint(0,len(mapcols[0])-1)
         if mapcols[y][x].solid or mapcols[y][x].occupied: continue
         else:
             monsters.append(Monster(name='kobold', sign='k', y=y, x=x))
-            if abs(monsters[-1].x - player.x) + abs(monsters[-1].y - player.y) < 13:
-                stdscr.move(y,x)
-                stdscr.addch(monsters[-1].sign)
             mapcols[y][x].occupied = monsters[-1]
-
+            monsters[-1].draw_if_visible(y, x)               
             
-
+      
 #######################################                        
 #####  MAIN GAME LOOP
 #######################################
@@ -168,6 +171,8 @@ def main(stdscr):
     game = True
     
     while game:
+    
+        stdscr.move(player.y, player.x) 	# center cursor on player
         
         key = stdscr.getkey()
         if key == 'e' and mapcols[player.y][player.x].loot: player.get_loot()          
